0542f3aafeb60d35762298346d3b8d4a04b3c4ef
Adrian Hansa <adrian.hansa@yahoo.com>
2021-08-30 17:23:55 +0100

    Created all accounts related reducers and updated the central store.

--- src/redux/reducers/accountReducers.js
+++ src/redux/reducers/accountReducers.js
@@ -2,6 +2,18 @@ import {
   CREATE_ACCOUNT_FAIL,
   CREATE_ACCOUNT_REQUEST,
   CREATE_ACCOUNT_SUCCESS,
+  DELETE_ACCOUNT_FAIL,
+  DELETE_ACCOUNT_REQUEST,
+  DELETE_ACCOUNT_SUCCESS,
+  GET_ACCOUNTS_FAIL,
+  GET_ACCOUNTS_REQUEST,
+  GET_ACCOUNTS_SUCCESS,
+  GET_ACCOUNT_FAIL,
+  GET_ACCOUNT_REQUEST,
+  GET_ACCOUNT_SUCCESS,
+  UPDATE_ACCOUNT_FAIL,
+  UPDATE_ACCOUNT_REQUEST,
+  UPDATE_ACCOUNT_SUCCESS,
 } from "../constants/accountConstants";
 
 export const createAccountReducer = (state = { account: {} }, action) => {
@@ -17,4 +29,60 @@ export const createAccountReducer = (state = { account: {} }, action) => {
   }
 };
 
-// export const updateActionReducer = ()
+export const updateAccountReducer = (
+  state = { accountUpdated: {} },
+  action
+) => {
+  switch (action.type) {
+    case UPDATE_ACCOUNT_REQUEST:
+      return { loading: true };
+    case UPDATE_ACCOUNT_SUCCESS:
+      return { loading: false, success: true, account: action.payload };
+    case UPDATE_ACCOUNT_FAIL:
+      return { loading: false, success: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const getAccountReducer = (state = { accountDetails: {} }, action) => {
+  switch (action.type) {
+    case GET_ACCOUNT_REQUEST:
+      return { loading: true };
+    case GET_ACCOUNT_SUCCESS:
+      return { loading: false, success: true, account: action.payload };
+    case GET_ACCOUNT_FAIL:
+      return { loading: false, success: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const getAccountsReducer = (state = { accounts: [] }, action) => {
+  switch (action.type) {
+    case GET_ACCOUNTS_REQUEST:
+      return { loading: true };
+    case GET_ACCOUNTS_SUCCESS:
+      return { loading: false, success: true, account: action.payload };
+    case GET_ACCOUNTS_FAIL:
+      return { loading: false, success: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const deleteAccountReducer = (
+  state = { accountDeleted: {} },
+  action
+) => {
+  switch (action.type) {
+    case DELETE_ACCOUNT_REQUEST:
+      return { loading: true };
+    case DELETE_ACCOUNT_SUCCESS:
+      return { loading: false, success: true, account: action.payload };
+    case DELETE_ACCOUNT_FAIL:
+      return { loading: false, success: false, error: action.payload };
+    default:
+      return state;
+  }
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -5,11 +5,21 @@ import { composeWithDevTools } from "redux-devtools-extension";
 const middleware = composeWithDevTools(applyMiddleware(thunk));
 
 import { authReducer } from "./reducers/userReducers";
-import { createAccountReducer } from "./reducers/accountReducers";
+import {
+  createAccountReducer,
+  updateAccountReducer,
+  deleteAccountReducer,
+  getAccountReducer,
+  getAccountsReducer,
+} from "./reducers/accountReducers";
 
 const reducerRoot = combineReducers({
   auth: authReducer,
   account: createAccountReducer,
+  accountDetails: getAccountReducer,
+  accountUpdated: updateAccountReducer,
+  accounts: getAccountsReducer,
+  accountDeleted: deleteAccountReducer,
 });
 
 const store = createStore(reducerRoot, middleware);
