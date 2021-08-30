2c5e90b84592c8d9ef90a289ac35dc7d49d64397
Adrian Hansa <adrian.hansa@yahoo.com>
2021-08-30 06:57:48 +0100

    Add icons and style them to the bottom tab navigation

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -11,13 +11,14 @@ import EditExpense from "../screens/expenses/EditExpense";
 import Accounts from "../screens/accounts/Accounts";
 import AddAccount from "../screens/accounts/AddAccount";
 import EditAccount from "../screens/accounts/EditAccount";
+import { FontAwesome } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 const ExpensesStack = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen component={Expenses} name="ExpensesScreen" />
       <Stack.Screen component={AddExpense} name="AddExpenseScreen" />
       <Stack.Screen component={EditExpense} name="EditExpenseScreen" />
@@ -27,7 +28,7 @@ const ExpensesStack = () => {
 
 const AccountStack = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen component={Accounts} name="Accounts" />
       <Stack.Screen component={AddAccount} name="AddAccount" />
       <Stack.Screen component={EditAccount} name="EditAccount" />
@@ -37,9 +38,9 @@ const AccountStack = () => {
 
 const AuthStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen component={Login} name="Login" />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen component={Register} name="Register" />
+      <Stack.Screen component={Login} name="Login" />
     </Stack.Navigator>
   );
 };
@@ -47,10 +48,28 @@ const AuthStack = () => {
 const AppNavigation = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="AuthTab" component={AuthStack} />
-        <Tab.Screen name="ExpensesTab" component={ExpensesStack} />
-        <Tab.Screen name="AccountsTab" component={AccountStack} />
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarActiveTintColor: "green",
+          tabBarIcon: ({ focused, color }) => {
+            focused ? (color = "green") : "black";
+            let iconName;
+            if (route.name === "Authenticate") {
+              iconName = "sign-in";
+            } else if (route.name === "Expenses") {
+              iconName = "money";
+            } else if (route.name === "Accounts") {
+              iconName = "list-alt";
+            }
+
+            return <FontAwesome name={iconName} size={24} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Authenticate" component={AuthStack} />
+        <Tab.Screen name="Expenses" component={ExpensesStack} />
+        <Tab.Screen name="Accounts" component={AccountStack} />
       </Tab.Navigator>
     </NavigationContainer>
   );
